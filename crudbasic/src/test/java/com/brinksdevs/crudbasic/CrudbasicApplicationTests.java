daf9119bbf0fb2c592d63e07e8a4defa78d65487
vinicius123131 <vini989073599@gmail.com>
2023-01-21 23:49:58 -0300

    modando de mavem pra gladlew e adicionando dependências

---
+++ crudbasic/src/test/java/com/brinksdevs/crudbasic/CrudbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brinksdevs.crudbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
